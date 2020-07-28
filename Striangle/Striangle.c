55b27e2dc52b17b8530d074d7f08ad510c64e383
Starkey <neron.seb@gmail.com>
2020-07-28 10:51:39 -0400

    Working Triangles at 1 FPS

--- Striangle/Striangle.c
+++ Striangle/Striangle.c
@@ -2,38 +2,88 @@
 
 /*******************************************************************************************
 *
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples in Notepad++, provided with default raylib installer package,
-*   just press F6 and run [raylib_compile_execute] script, it will compile and execute.
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on [C:\raylib\raylib\examples] directory and
-*   raylib official webpage: [www.raylib.com]
-*
-*   Enjoy using raylib. :)
+*   raylib [shapes] example - Draw basic shapes 2d (rectangle, circle, line...)
 *
 *   This example has been created using raylib 1.0 (www.raylib.com)
 *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
 *
-*   Copyright (c) 2013-2020 Ramon Santamaria (@raysan5)
+*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
 *
 ********************************************************************************************/
 
 #include "raylib.h"
 
+typedef struct Triangle
+{
+    Vector2 vu;
+    Vector2 vl;
+    Vector2 vr;
+    Color color;
+};
+typedef struct Triangle Triangle;
+
+
+Triangle* SubdivideTris(Triangle* ptri, int *size)
+{
+    int new_size = 3 * *size;
+    int i = 0;
+    Triangle* new_ptri = (Triangle *) malloc(new_size * sizeof(Triangle));
+    Triangle tri,tri_up,tri_left,tri_right;
+    Vector2 mdown, mleft, mright;
+
+    for (i = 0; i < *size; i++)
+    {
+        tri = *(ptri + i);
+        mdown = (Vector2) {tri.vu.x, tri.vl.y};
+        mleft = (Vector2) {tri.vl.x + (mdown.x-tri.vl.x)/2, mdown.y + (tri.vu.y-mdown.y)/2};
+        mright = (Vector2) {mdown.x + (tri.vr.x - mdown.x) / 2, mleft.y};
+
+        tri_up.vu = tri.vu;
+        tri_up.vl = mleft;
+        tri_up.vr = mright;
+        
+        tri_left.vu = mleft;
+        tri_left.vl = tri.vl;
+        tri_left.vr = mdown;
+
+        tri_right.vu = mright;
+        tri_right.vl = mdown;
+        tri_right.vr = tri.vr;
+
+        *(new_ptri + (3 * i)) = tri_up;
+        *(new_ptri + (3 * i) + 1) = tri_left;
+        *(new_ptri + (3 * i) + 2) = tri_right;
+    }
+
+    free(ptri);
+    *size = new_size;
+
+    return new_ptri;
+}
+
+
+
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1800;
+    const int screenHeight = 1000;
+    Triangle tri;
+    Triangle* ptri;
+    int size = 1;
+    int i = 0;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    tri.vu = (Vector2){ screenWidth / 2, 0 };
+    tri.vl = (Vector2){ 0, screenHeight };
+    tri.vr = (Vector2){ screenWidth, screenHeight };
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    ptri = (Triangle*)malloc(sizeof(Triangle));
+    *ptri = tri;
+
+    InitWindow(screenWidth, screenHeight, "raylib [shapes] example - basic shapes drawing");
+
+    SetTargetFPS(1);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
@@ -50,11 +100,19 @@ int main(void)
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        for (i = 0; i < size; i++)
+        {
+            DrawTriangle((ptri+i)->vu, (ptri + i)->vl, (ptri + i)->vr, VIOLET);
+        }
+
+        ptri = SubdivideTris(ptri, &size);
+        
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
+
+    free(ptri);
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
